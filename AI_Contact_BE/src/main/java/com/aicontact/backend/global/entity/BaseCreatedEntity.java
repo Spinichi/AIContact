04c0686ee33f223b496ccbbf1a95285eb3f7ee7b
spinichi <spinichi@naver.com>
2025-07-23 15:56:10 +0900

    feat: BaseEntity 제약조건 추가 [S13P11A702-65]

--- AI_Contact_BE/src/main/java/com/aicontact/backend/global/entity/BaseCreatedEntity.java
+++ AI_Contact_BE/src/main/java/com/aicontact/backend/global/entity/BaseCreatedEntity.java
@@ -1,5 +1,6 @@
 package com.aicontact.backend.global.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -14,5 +15,6 @@ import java.time.LocalDateTime;
 public abstract class BaseCreatedEntity {
 
     @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
 }
--- AI_Contact_BE/src/main/java/com/aicontact/backend/global/entity/BaseTimeEntity.java
+++ AI_Contact_BE/src/main/java/com/aicontact/backend/global/entity/BaseTimeEntity.java
@@ -1,18 +1,17 @@
 package com.aicontact.backend.global.entity;
 
-import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity extends BaseCreatedEntity {
 
     @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
