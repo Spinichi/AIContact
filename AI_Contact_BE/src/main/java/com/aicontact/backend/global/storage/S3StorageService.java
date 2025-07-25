86cded795f3b08f21a91786fc3299c7fd319e370
spinichi <spinichi@naver.com>
2025-07-25 11:06:47 +0900

    refactor: 기존 메서드를 범용 업로드 로직으로 통일 [S13P11A702-65]

--- AI_Contact_BE/src/main/java/com/aicontact/backend/global/storage/S3StorageService.java
+++ AI_Contact_BE/src/main/java/com/aicontact/backend/global/storage/S3StorageService.java
@@ -3,56 +3,47 @@ package com.aicontact.backend.global.storage;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
-import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
-public class S3StorageService implements StorageService {
+public class S3StorageService {
 
     private final AmazonS3Client s3Client;
-    private final String bucketName = System.getProperty("cloud.aws.s3.bucket");
-    // 또는 @Value("${cloud.aws.s3.bucket}") 주입
 
-    @Override
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    /**
+     * 업로드: MultipartFile 원본
+     */
     public String upload(MultipartFile file, String key) throws IOException {
-        ObjectMetadata meta = new ObjectMetadata();
-        meta.setContentType(file.getContentType());
-        meta.setContentLength(file.getSize());
+        return upload(file.getBytes(), key, file.getContentType());
+    }
 
-        s3Client.putObject(bucketName, key, file.getInputStream(), meta);
+    /**
+     * 범용 업로드: 바이트 배열 + MIME 타입
+     */
+    public String upload(byte[] data, String key, String contentType) {
+        ObjectMetadata meta = new ObjectMetadata();
+        meta.setContentType(contentType);
+        meta.setContentLength(data.length);
+
+        ByteArrayInputStream input = new ByteArrayInputStream(data);
+        s3Client.putObject(bucketName, key, input, meta);
         return s3Client.getUrl(bucketName, key).toString();
     }
 
-    @Override
-    public String generateThumbnailAndUpload(MultipartFile file, String thumbKey) throws IOException {
-        // 1) 원본 이미지를 Thumbnails로 리사이징
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        Thumbnails.of(file.getInputStream())
-                .size(200, 200)
-                .outputFormat("jpg")
-                .toOutputStream(os);
-
-        byte[] thumbBytes = os.toByteArray();
-        ByteArrayInputStream thumbInput = new ByteArrayInputStream(thumbBytes);
-
-        // 2) 메타데이터 세팅
-        ObjectMetadata meta = new ObjectMetadata();
-        meta.setContentType("image/jpeg");
-        meta.setContentLength(thumbBytes.length);
-
-        // 3) S3 업로드
-        s3Client.putObject(bucketName, thumbKey, thumbInput, meta);
-        return s3Client.getUrl(bucketName, thumbKey).toString();
-    }
-
-    @Override
+    /**
+     * S3 객체 삭제
+     */
     public void delete(String key) {
         s3Client.deleteObject(bucketName, key);
     }
 }
+
