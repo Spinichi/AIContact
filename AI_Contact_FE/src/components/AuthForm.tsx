888b80a13fb542f95105333ab3ecd6f92727c496
ldg1103 <ldg1103@naver.com>
2025-07-25 17:09:37 +0900

    feat : 레이아웃 애니메이션 구현[S13P11A702-16]

--- AI_Contact_FE/src/components/AuthForm.tsx
+++ AI_Contact_FE/src/components/AuthForm.tsx
@@ -1,9 +1,14 @@
+import '../styles/AuthFormPanel.css';
 
 interface AuthFormProps {
     position : 'left' | 'right';
     onFormChange : () => void;
 }
 
-export default function AuthForm() {
-    return (<></>);
+export default function AuthForm({position, onFormChange} : AuthFormProps) {
+    return (
+        <div className={`auth-form-panel ${position}`} onClick={onFormChange}>
+
+        </div>
+    );
 }
--- AI_Contact_FE/src/pages/AuthPage.tsx
+++ AI_Contact_FE/src/pages/AuthPage.tsx
@@ -4,14 +4,14 @@ import AuthBackground from '../components/AuthBackground.tsx';
 import AuthForm from '../components/AuthForm';
 
 export default function AuthPage() {
-    const [isSignUp, setIsSignUp] = useState(true);
+    const [isSignUp, setIsSignUp] = useState(false);
 
     return(
         <div className="main-layout">
             <AuthBackground />
-             
-            {/* <AuthForm position = {isSignUp ? 'right' : 'left'} 
-            onFormChange={()=>setIsSignUp(isSignUp => !isSignUp)} /> */}
+            
+            <AuthForm position = {isSignUp ? 'right' : 'left'} 
+            onFormChange={()=>setIsSignUp(isSignUp => !isSignUp)} />
 
         </div>
     );
