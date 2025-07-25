dc450729cdc48e12ba7ec9f5984b0500749e9b5f
ldg1103 <ldg1103@naver.com>
2025-07-25 17:50:13 +0900

    feat : 회원가입 및 로그인 폼 구현[S13P11A702-16]

--- AI_Contact_FE/src/components/AuthForm.tsx
+++ AI_Contact_FE/src/components/AuthForm.tsx
@@ -1,14 +1,45 @@
 import '../styles/AuthFormPanel.css';
 
 interface AuthFormProps {
+    isSignUp : boolean;
     position : 'left' | 'right';
     onFormChange : () => void;
 }
 
-export default function AuthForm({position, onFormChange} : AuthFormProps) {
+export default function AuthForm({isSignUp, position, onFormChange} : AuthFormProps) {
     return (
-        <div className={`auth-form-panel ${position}`} onClick={onFormChange}>
-
+        <div className={`auth-form-panel ${position}`}>
+            {isSignUp ? (
+                <LoginForm onToggle={onFormChange} />
+                ) : (
+                <SignUpForm onToggle={onFormChange} />
+                )}
         </div>
     );
+}
+
+function LoginForm({onToggle}){
+  return (
+    <div className="form-box">
+      <h2>AI Contact</h2>
+      <input type="text" placeholder="아이디" />
+      <input type="password" placeholder="비밀번호" />
+      <button>로그인</button>
+      <p onClick={onToggle} className="toggle-text">아직 회원이 아니신가요? 회원가입</p>
+    </div>
+  );  
+}
+
+function SignUpForm({ onToggle }) {
+  return (
+    <div className="form-box">
+      <h2>AI Contact</h2>
+      <input type="text" placeholder="아이디" />
+      <input type="email" placeholder="이메일" />
+      <input type="password" placeholder="비밀번호" />
+      <input type="password" placeholder="비밀번호 확인" />
+      <button>회원가입</button>
+      <p onClick={onToggle} className="toggle-text">이미 계정이 있으신가요? 로그인</p>
+    </div>
+  );
 }
