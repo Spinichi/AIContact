09dcc7a1fb65e5a58ed584acbf5628c6ceec93c5
노다빈 <scnoh02@naver.com>
2025-07-28 14:46:45 +0900

    feat: [FE] OpenVidu WebRTC 구현[S13P11A702-62]

--- AI_Contact_FE/src/App.tsx
+++ AI_Contact_FE/src/App.tsx
@@ -12,6 +12,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import MainPage from "./pages/MainPage"; // 기존 메인 페이지
 import AuthPage from "./pages/AuthPage"; // 회원가입 페이지
+import WebRtcPage from "./webrtc/WebRtcPage"; // 회원가입 페이지
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/AuthPage" element={<AuthPage />} />
+        <Route path="/webrtc" element={<WebRtcPage />} />
       </Routes>
     </Router>
   );
