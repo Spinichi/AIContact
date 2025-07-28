ae513e646d07e45f324f3de4dd5b051206088651
ldg1103 <ldg1103@naver.com>
2025-07-28 15:08:27 +0900

    update : 회원가입 폼 이미지를 첨부할 수 있게 수정[S13P11A702-16]

--- AI_Contact_FE/src/components/auth/ProfileForm.tsx
+++ AI_Contact_FE/src/components/auth/ProfileForm.tsx
@@ -1,13 +1,34 @@
 import edit from '../../assets/icons/edit.png';
 
+import {useState} from 'react';
+
 export default function ProfileForm(){
+
+    const [imagePreview, setImagePreview] = useState('');
+
+    const uploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+  
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const previewUrl = URL.createObjectURL(file);
+  
+    setImagePreview(previewUrl);
+    };
+
     return(
         <>
             <h2>프로필 입력</h2>
             <form className="form-box">
-                <div className="profile">
-                    <img src={edit}  className="image-edit"/>
-                </div>
+                    <div className="profile">
+                        <label htmlFor="profile-image">
+                            {imagePreview && (<img src={imagePreview} className="image-preview"/>)}
+                            <img src={edit}  className="image-edit"/>
+                        </label>
+                    </div>
+                <input type="file" name="image" 
+                id="profile-image" accept="image/*" 
+                style={{'display':'none'}} onChange={uploadImage} />
                 <input type="text" placeholder="이름" />
                 <label>생년월일</label>
                 <div className="birth">
