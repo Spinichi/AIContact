787a0b0f026fa176dc038fa0e8df83200df78be9
DESKTOP-DLOPF19\SSAFY <mim_vely@naver.com>
2025-07-28 16:40:20 +0900

    feat: 네브바 버튼 클릭 수정 [S13P11A702-15]

--- AI_Contact_FE/src/components/Sidebar.tsx
+++ AI_Contact_FE/src/components/Sidebar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/Sidebar.css'; 
 import Logo from './Logo.tsx';
+import {useNavigate} from 'react-router-dom';
 
 // 컴포넌트 만들기
 export default function Sidebar() {
+  const navigate = useNavigate();
   return (
     // 전체 사이드바를 감싸는 컨테이너
     <aside className="sidebar">
@@ -15,25 +17,39 @@ export default function Sidebar() {
         <nav>
           <ul>
             {/* 현재 경로가 "/"일 때 active */}
-            <li className={location.pathname === '/' ? 'active' : ''}>
-              <Link to="/">아이</Link>
+            <li
+              className={location.pathname === '/' ? 'active' : ''}
+              onClick={() => navigate('/')}
+            >
+              아이
             </li>
 
-            {/* 현재 경로가 "/gallery"일 때 active */}
-            <li className={location.pathname === '/gallery' ? 'active' : ''}>
-              <Link to="/gallery">갤러리</Link>
+            <li
+              className={location.pathname === '/gallery' ? 'active' : ''}
+              onClick={() => navigate('/gallery')}
+            >
+              갤러리
             </li>
 
-            {/* 현재 경로가 "/calendar"일 때 active */}
-            <li className={location.pathname === '/calendar' ? 'active' : ''}>
-              <Link to="/calendar">캘린더</Link>
+            <li
+              className={location.pathname === '/calendar' ? 'active' : ''}
+              onClick={() => navigate('/calendar')}
+            >
+              캘린더
             </li>
 
-            <li className={location.pathname === '/dictionary' ? 'active' : ''}>
-              <Link to="/dictionary">애칭 백과사전</Link>
+            <li
+              className={location.pathname === '/dictionary' ? 'active' : ''}
+              onClick={() => navigate('/dictionary')}
+            >
+              애칭 백과사전
             </li>
-            <li className={location.pathname === '/mypage' ? 'active' : ''}>
-              <Link to="/mypage">마이페이지</Link>
+
+            <li
+              className={location.pathname === '/mypage' ? 'active' : ''}
+              onClick={() => navigate('/mypage')}
+            >
+              마이페이지
             </li>
           </ul>
         </nav>
