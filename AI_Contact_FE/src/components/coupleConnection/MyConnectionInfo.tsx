5ce75c9925b667e57fab0bd85c019501c8da16ce
ldg1103 <ldg1103@naver.com>
2025-07-28 16:12:50 +0900

    fix : 사이드바 오류 수정

--- AI_Contact_FE/src/components/coupleConnection/MyConnectionInfo.tsx
+++ AI_Contact_FE/src/components/coupleConnection/MyConnectionInfo.tsx
@@ -2,8 +2,8 @@ import '../../styles/CoupleConnection.css';
 
 export default function MyConnectionInfo(){
     return(
-        <div className="container">
-            <div className="profile-image"></div>
+        <div className="connection-container">
+            <div className="profile-container"></div>
             <p className="code-label">당신의 코드</p>
             <p className="code">s1a3fgfiy</p>
             <button>공유하기</button>
