d4b3d94da3e5496d1f28e0a6b259ad5a5f32de3e
ldg1103 <ldg1103@naver.com>
2025-07-29 22:01:34 +0900

    chore: 프로필 사진 로직 변경 및 안쓰는 파일 제거[S13P11A702-20]

--- AI_Contact_FE/src/components/calendar/CalendarDetail.tsx
+++ AI_Contact_FE/src/components/calendar/CalendarDetail.tsx
@@ -6,15 +6,15 @@ interface CalendarDetailProps{
     events : Array<object>;
 }
 
-export default function CalendarDetail({dateInfo, events}: CalendarDetailProps){
+export default function CalendarDetail({dateInfo}: CalendarDetailProps){
 
     const days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
     const calendarEvents = [
-        {title:"포비 산책", time : '13:00', player:["이지은"], index:1},
-        {title:"포비 밥주기", time: "14:00", player:["이종석"], index:2},
-        {title:"포비 놀기", time: "17:30", player:["이지은"], index:2},
-        {title:"포비 포비 포비", time: "19:00", player:["이지은"], index:2},
-        {title:"포비 산책", time: "21:50", player:["이지은", "이종석"], index:2},
+        {title:"포비 산책", time : '13:00', play_index:1, index:1},
+        {title:"포비 밥주기", time: "14:00", play_index:2, index:2},
+        {title:"포비 놀기", time: "17:30", play_index:1, index:2},
+        {title:"포비 포비 포비", time: "19:00", play_index:2, index:2},
+        {title:"포비 산책", time: "21:50", play_index:3, index:2},
     ]
 
     return(
@@ -33,9 +33,8 @@ export default function CalendarDetail({dateInfo, events}: CalendarDetailProps){
                     <div className="event-list">
                         <p className="event-time">{obj.time}</p>
                         <div className="event-profile-list">
-                            {obj.player.map((playerName, playerIndex) => (
-                                <div className="event-profile" key={`${obj.index}-${playerIndex}`} />
-                            ))}
+                            {((obj.play_index&1)!=0) && <div className="event-profile first" />}
+                            {(((obj.play_index>>1)&1)!=0) && <div className="event-profile second" />}
                         </div>
                         
                         <p className="event-title">{obj.title}</p>
--- AI_Contact_FE/src/components/calendar/CalendarDetailProps.tsx
+++
@@ -1,4 +0,0 @@
-export interface CalendarDetailProps {
-    dateInfo: Date;
-    events: Array;
-}
