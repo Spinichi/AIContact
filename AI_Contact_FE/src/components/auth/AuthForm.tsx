c44d7b270f0ef895c8576ce0eae75681aabb978e
노다빈 <scnoh02@naver.com>
2025-07-29 09:52:37 +0900

    feat: auth 페이지 ArrowLeft png → svg 변경 [S13P11A702-74]

--- AI_Contact_FE/src/components/auth/AuthForm.tsx
+++ AI_Contact_FE/src/components/auth/AuthForm.tsx
@@ -1,39 +1,41 @@
-import '../../styles/AuthFormPanel.css';
+import "../../styles/AuthFormPanel.css";
 
-import arrowLeft from '../../assets/icons/ArrowLeft.png'
-import LoginForm from './LoginForm';
-import SignUpForm from './SignUpForm';
-import ProfileForm from './ProfileForm';
-import FormTitle from './FormTitle';
+import arrowLeft from "../../assets/icons/ArrowLeft.svg";
+import FormTitle from "./FormTitle";
+import LoginForm from "./LoginForm";
+import ProfileForm from "./ProfileForm";
 
 interface AuthFormProps {
-    isSignUp : boolean;
-    position : 'left' | 'right';
-    onFormChange : () => void;
+  isSignUp: boolean;
+  position: "left" | "right";
+  onFormChange: () => void;
 }
 
-export default function AuthForm({isSignUp, position, onFormChange} : AuthFormProps) {
-    return (
-        <div className={`auth-form-panel ${position}`}>
-          
-            {isSignUp ? (
-              //<SignUpForm onToggle={onFormChange} />
-              <ProfileForm />
-            ) : (
-              <>
-                <LeftArrow />
-                <FormTitle />
-                <LoginForm onToggle={onFormChange} />
-              </>
-            )}
-        </div>
-    );
+export default function AuthForm({
+  isSignUp,
+  position,
+  onFormChange,
+}: AuthFormProps) {
+  return (
+    <div className={`auth-form-panel ${position}`}>
+      {isSignUp ? (
+        //<SignUpForm onToggle={onFormChange} />
+        <ProfileForm />
+      ) : (
+        <>
+          <LeftArrow />
+          <FormTitle />
+          <LoginForm onToggle={onFormChange} />
+        </>
+      )}
+    </div>
+  );
 }
 
-function LeftArrow(){
-  return(
+function LeftArrow() {
+  return (
     <div className="arrow-left">
       <img src={arrowLeft} />
     </div>
-  )
+  );
 }
--- AI_Contact_FE/src/pages/AuthPage.tsx
+++ AI_Contact_FE/src/pages/AuthPage.tsx
@@ -1,20 +1,19 @@
-import '../styles/MainPages.css';
-import React, { useState } from 'react';
-import AuthBackground from '../components/auth/AuthBackground.tsx';
-import AuthForm from '../components/auth/AuthForm.tsx';
+import { useState } from "react";
+import AuthBackground from "../components/auth/AuthBackground.tsx";
+import AuthForm from "../components/auth/AuthForm.tsx";
+import "../styles/MainPages.css";
 
 export default function AuthPage() {
-    const [isSignUp, setIsSignUp] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
 
-    return(
-        <div className="main-layout">
-            <AuthBackground />
-            <AuthForm 
-                position={isSignUp ? 'right' : 'left'}
-                onFormChange={() => setIsSignUp(prev => !prev)} 
-                isSignUp={isSignUp} 
-            />
-
-        </div>
-    );
+  return (
+    <div className="main-layout">
+      <AuthBackground />
+      <AuthForm
+        position={isSignUp ? "right" : "left"}
+        onFormChange={() => setIsSignUp((prev) => !prev)}
+        isSignUp={isSignUp}
+      />
+    </div>
+  );
 }
