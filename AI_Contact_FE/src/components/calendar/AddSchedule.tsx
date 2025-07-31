9eba83f83e965d1eba8e06da0ba5877ffa3bdf26
ldg1103 <ldg1103@naver.com>
2025-07-31 12:40:00 +0900

    feat: 스케쥴 이름 수정 및 기능 개선 [S13P11A702-20]

--- AI_Contact_FE/src/components/calendar/AddSchedule.tsx
+++ AI_Contact_FE/src/components/calendar/AddSchedule.tsx
@@ -1,27 +1,25 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel } from 'swiper/modules';
 
-import '../../styles/AddCalendarEvent.css';
+import '../../styles/AddSchedule.css';
 
 import 'swiper/css';
 
-interface AddCalendarEventProps{
+interface AddScheduleProps{
     dateInfo : Date;
     onCancel : () => void;
 }
 
-export default function AddCalendarEvent({onCancel, dateInfo} : AddCalendarEventProps){
+export default function AddSchedule({onCancel, dateInfo} : AddScheduleProps){
 
     const days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
 
-    const formatNumber = (num) => String(num).padStart(2, '0');
-    const hours = Array.from({ length: 24 }, (_, i) => formatNumber(i));
+    const formatNumber = (num: number) => String(num).padStart(2, '0');
+    const hours = Array.from({ length: 24 }, (_, i) => formatNumber((i + 9) % 24));
     const minutes = Array.from({ length: 60 }, (_, i) => formatNumber(i));
 
-    const profileFormat = [1,2,3];
-
         return(
-        <div className="calendar-modal add-calendar-event">
+        <div className="calendar-modal add-schedule">
             <div className="modal-header">
                 <div className="date">
                     <div className="monthday">{dateInfo.getMonth()+1}월 {dateInfo.getDate()}일</div>
@@ -29,14 +27,15 @@ export default function AddCalendarEvent({onCancel, dateInfo} : AddCalendarEvent
                 </div>
             </div>
             <form className="modal-body">
-                <input className="event-title" placeholder='제목'></input>
+                <input className="schedule-title" placeholder='제목'></input>
                 <div className="modal-content">
-                    <div className="timer">
-                        <p className="timer-title">시간</p>
+                    <div className="section timer">
+                        <p className="title">시간</p>
                         <div className = "timer-layout">
                             <Swiper
                             centeredSlides={true}
                             slidesPerView={3}
+                            mousewheel={true}
                             loop={true}
                             modules={[Mousewheel]}
                             direction={'vertical'}
@@ -51,6 +50,7 @@ export default function AddCalendarEvent({onCancel, dateInfo} : AddCalendarEvent
                             centeredSlides={true}
                             slidesPerView={3}
                             loop={true}
+                            mousewheel={true}
                             modules={[Mousewheel]}
                             direction={'vertical'}
                         className="mySwiper">
@@ -64,15 +64,9 @@ export default function AddCalendarEvent({onCancel, dateInfo} : AddCalendarEvent
                         
                     <div />
                     </div>
-                    <div className="profile-selection">
-                        {
-                           profileFormat.map((num) => (
-                            <div className='profile-list'>
-                               {((num&1)!=0) && <div className="event-profile first" />}
-                            {(((num>>1)&1)!=0) && <div className="event-profile second" />} 
-                            </div>
-                           ))
-                        }
+                    <div className="section memo">
+                        <p className="title">메모</p>
+                        <textarea className="memo" placeholder='메모를 입력하세요.'></textarea>
                     </div>
                 </div>
                 <div className="modal-footer">
