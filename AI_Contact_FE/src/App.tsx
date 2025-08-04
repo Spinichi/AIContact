7960bcc7f000785c3cef08146b0476c170cdcf64
ldg1103 <ldg1103@naver.com>
2025-08-04 13:09:00 +0900

    feat: 온보딩 페이지 최상단 및  목업 화면 구현[S13P11A702-26]

--- AI_Contact_FE/src/App.tsx
+++ AI_Contact_FE/src/App.tsx
@@ -11,6 +11,7 @@ import MyPage from "./pages/MyPage";
 import TalkPage from "./pages/TalkRoomPage";
 import CartoonResultPage from "./pages/CartoonResultPage";
 import WebRtcPage from "./pages/WebRtcPage";
+import OnBoardingPage from "./pages/OnBoardingPage";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/dictionary" element={<DictionaryPage />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/index" element={<OnBoardingPage />} />
         {/* </Route> */}
       </Routes>
     </Router>
