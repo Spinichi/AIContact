44a7eda3044c2d2dace7405fa0f6ee04bf843115
김소연 <sykim0659@naver.com>
2025-08-08 13:37:55 +0900

    feat:chat api 연결[S13P11A702-85]

--- AI_Contact_FE/vite.config.ts
+++ AI_Contact_FE/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr' 
+import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   assetsInclude: ['**/*.glb'],
+  define: {
+    global: 'window',
+  },
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        ws: true, // ✅ WebSocket 프록시 설정
+      }
+    }
+  }
 })
